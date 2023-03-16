d2e3a8266f58a9ab4f98023316cbd10ebc0d343b
xSIayer <ahmedwael130@gmail.com>
2023-03-16 16:38:59 -0700

    asd

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 int main(void)
 {
-	char x [] = "Programming is like building a multilingual puzzle"
+	char x [] = "Programming is like building a multilingual puzzle";
 		putchar(x);
 	return(0);
 }
