89905fe9243848452c5262efdcc5b918815c188e
xSIayer <ahmedwael130@gmail.com>
2023-03-17 15:51:21 -0700

    asd

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ *main - A c program for printing
+ *Return: 0 (Success)
+ */
+int main(void)
+{
+	char x;
+
+	for (x = 'a'; x <= 'z'; x++)
+	{
+		if (x == 'q' || x == 'e')
+			continue;
+		putchar(x);
+	}
+	putchar('\n');
+	return (0);
+}
