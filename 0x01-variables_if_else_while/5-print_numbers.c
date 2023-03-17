c2a4085db4c0f8e2fca5c95af68a41ff8655348c
xSIayer <ahmedwael130@gmail.com>
2023-03-17 15:54:34 -0700

    asd

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ *main - A c program for printing
+ *Return: 0 (Success)
+ */
+int main(void)
+{
+	int x;
+
+	for (x = 0; x < 10; x++)
+	{
+		printf("%d",x);
+	}
+	putchar('\n');
+	return (0);
+}
