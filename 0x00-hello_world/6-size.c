03525256c32c97d62a3e66a8781051a20671ac8b
xSIayer <ahmedwael130@gmail.com>
2023-03-16 17:29:24 -0700

    asd

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - A c program for printing size of data
+ * Return: 0 (Success)
+ */
+int main(void) 
+{
+	printf("Size of a char: %ld.\n",sizeof(char));
+	 printf("Size of a int: %ld.\n",sizeof(int));
+		  printf("Size of a int: %ld.\n",sizeof(long int));
+		   printf("Size of a long long int: %ld.\n",sizeof(long long int));
+		    printf("Size of a float: %ld.\n",sizeof(float));
+return(0);			
+}
