df5cc3d20ff6317485cc42952240e113ff4f32ec
xSIayer <ahmedwael130@gmail.com>
2023-03-17 16:23:18 -0700

    das

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ *main - A c program for printing
+ *Return: 0 (Success)
+ */
+int main(void)
+{
+	int x;
+
+	for (x = 48; x < 58; x++)
+	{
+		putchar(x);
+		if (x==57)
+			break;
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
