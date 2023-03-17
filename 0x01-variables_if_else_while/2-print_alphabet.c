2f9b95545332303a042af82d9745b4ac9918fdf4
xSIayer <ahmedwael130@gmail.com>
2023-03-17 15:35:46 -0700

    asd

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ *main - A c program for printing
+ *Return: 0 (Success)
+ */
+int main(void)
+{
+	char x;
+
+	for (x = 'a'; x <= 'z'; x++)
+	{
+		putchar(x);
+	}
+	return (0);
+}
