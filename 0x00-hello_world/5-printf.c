c2656f5781cfc4eaca426d36c30f87b286c3184d
xSIayer <ahmedwael130@gmail.com>
2023-03-16 17:18:59 -0700

    asd

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,4 +1,4 @@
-#include<stdio.h>
+#include <stdio.h>
 /**
  *main - A c code for printing
  *Return: 0 (Success)
@@ -6,5 +6,5 @@
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return(0);
+	return (0);
 }
