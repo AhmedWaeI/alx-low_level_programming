436d09907933250d42a9acb77b9aecb8e4317f87
xSIayer <ahmedwael130@gmail.com>
2023-03-17 16:10:07 -0700

    das

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ *main - A c program for printing
+ *Return: 0 (Success)
+ */
+int main(void)
+{
+	char x;
+
+	char z;
+
+	for (x = 'z'; x >= 'a'; x--)
+	{
+		putchar(x);
+	}
+	putchar('\n');
+	return (0);
+}
