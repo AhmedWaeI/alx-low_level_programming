8142db00b4e7f944dcef68defaacfbbde1609245
xSIayer <ahmedwael130@gmail.com>
2023-03-21 06:03:09 -0700

    asd

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -9,14 +9,14 @@ int print_sign(int n)
 {
 	if (n < 0)
 	{
-		putchar('-');
+		_putchar('-');
 		return (-1);
 	}
 	if (n == 0)
 	{
-		putchar('0');
+		_putchar('0');
 		return (0);
 	}
-	putchar('+');
+	_putchar('+');
 	return (1);
 }
