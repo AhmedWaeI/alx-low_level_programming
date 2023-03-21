fa567535ec6e50cf95258f6da7e46a3405330e65
xSIayer <ahmedwael130@gmail.com>
2023-03-21 07:16:54 -0700

    asd

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -12,16 +12,13 @@ void jack_bauer(void)
 	
 	int m;
 
-	h=0;
+	h = 0;
 	
 	for (i = 0; i < 1440; i++)
 	{
 		m = i % 60;
-		if (i % 60 == 0)
-		{
-			h = h + 1;
-			m = 0;
-		}
+		
+		h = i / 60;
 		if (m < 10)
 		{
 			if (h < 10)
