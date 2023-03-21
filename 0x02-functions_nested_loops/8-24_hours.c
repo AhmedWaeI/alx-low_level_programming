74a55f4e57d928858a537b72008a568ca7ff3977
xSIayer <ahmedwael130@gmail.com>
2023-03-21 07:08:28 -0700

    asd

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,37 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * jack_bauer - the function
+ * Return: 0
+ */
+void jack_bauer(void)
+{
+	int i;
+
+	int h;
+	
+	int m;
+	
+	for (i = 0; i < 1440; i++)
+	{
+		m = i % 60;
+		if (i % 60 == 0)
+		{
+			h = h + 1;
+			m = 0;
+		}
+		if (m < 10)
+		{
+			if (h < 10)
+				printf("0%d:0%d", h, m);
+
+			printf("%d:0%d", h, m);
+		}
+		if (m >= 10)
+		{
+			if (h < 10)
+				printf("0%d:%d", h, m);
+			printf("%d:%d", h, m);
+		}
+	}
+}
