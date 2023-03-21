ea9661768925a5da2618065171331d135cc8d9a5
xSIayer <ahmedwael130@gmail.com>
2023-03-21 07:44:03 -0700

    asd

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -19,14 +19,14 @@ void times_table(void)
 		for (j = 0; j < 10; j++)
 		{
 			x = i * j;
-			z= (j + 1) * i;
+			z = (j + 1) * i;
 			if (j == 9)
 			{
 				 printf("%d", x);
 			}
 			else
 			{
-				if(z  > 9)
+				if (z  > 9)
 				{
 					printf("%d, ", x);
 				}
