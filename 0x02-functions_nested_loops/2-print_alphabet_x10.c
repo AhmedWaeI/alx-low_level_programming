30fdc6d83cd9e1dad6bd74545ecec6631382641a
xSIayer <ahmedwael130@gmail.com>
2023-03-21 05:23:57 -0700

    asd

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *print_alphabet_x10 - prints the alphabets
+ *Return: 0 (Success)
+ */
+void print_alphabet_x10(void)
+{
+	int j;
+
+	for (j = 0; j<10; j++)
+	{
+		char c;
+
+		for(c = 'a'; c <= 'z'; c++)
+		{
+			putchar(c)
+		}
+		putchar('\n');
+	}
+}
