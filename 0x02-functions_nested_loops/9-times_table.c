c6802712d99e29667a4190688c1e1ad404b5ffc0
xSIayer <ahmedwael130@gmail.com>
2023-03-21 07:30:06 -0700

    asd

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * times_table - prints the time table
+ */
+
+void times_table(void)
+{
+	int i;
+	
+	int j;
+
+	int x;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			x = i * j
+			printf("%d,  ", x);
+		}
+	}
+}	
