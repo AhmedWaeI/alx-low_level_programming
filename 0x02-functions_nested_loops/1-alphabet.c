b6e0cd405f4d245a03ae81208750073b23226494
xSIayer <ahmedwael130@gmail.com>
2023-03-21 04:51:39 -0700

    asd

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ *main - A c program for printing
+ *Return: 0 (Success)
+ */
+void print_alphabet(void)
+{
+	for(char i = 'a'; i <= 'z'; i++)
+        {
+		putchar(i);
+        }
+	putchar('\n');
+}
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
