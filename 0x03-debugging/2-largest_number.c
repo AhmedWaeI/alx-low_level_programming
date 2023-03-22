b7e8002bd7e4def57e35e3da6b60b6203f58fe14
xSIayer <ahmedwael130@gmail.com>
2023-03-22 15:21:17 -0700

    asd

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -10,24 +10,29 @@
 
 int largest_number(int a, int b, int c)
 {
-int largest;
+	int largest;
 
-if (a > b && a > c)
-{
-largest = a;
-}
-else if (a > b && c > a)
-{
-largest = c;
-}
-else if (b > c)
-{
-largest = b;
-}
-else
-{
-largest = c;
-}
-
-return (largest);
+	if (a < b)
+	{
+		if (b > c)
+		{
+			largest = b;
+		}
+		else
+		{
+			largest = c;
+		}
+	}
+	else if (b <= a)
+	{
+		if (a > c)
+		{
+			largest = a;
+		}
+		else
+		{
+			largest = c;
+		}
+	}
+	return (largest);
 }
