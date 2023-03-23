5510b5c6a1af8f10ea5d3a8e0aa7400f645fe1c0
xSIayer <ahmedwael130@gmail.com>
2023-03-23 16:03:10 -0700

    asd

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_square - prints a square, followed by a new line;
+ * @size: size of the square
+ */
+void print_square(int size)
+{
+	int i;
+
+	int j;
+
+	for (i = 0; i < size; i++)
+	{
+		_putchar(' ');
+	}
+	 _putchar('\');
+}
