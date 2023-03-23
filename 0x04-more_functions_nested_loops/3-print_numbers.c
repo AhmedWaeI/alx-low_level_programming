d654843a4cf819ec40d11c42465b8fd0c636db1a
xSIayer <ahmedwael130@gmail.com>
2023-03-23 15:07:39 -0700

    asd

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_numbers - check the code for Holberton School students.
+ *
+ * Return: zero.
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d", i);
+	}
+	putchar('\n');
+}
