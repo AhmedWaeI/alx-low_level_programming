beb0e9606dd540246d98c06863a7ba096171241c
xSIayer <ahmedwael130@gmail.com>
2023-03-23 13:59:58 -0700

    asd

--- 0x04-more_functions_nested_loops/0-main.c
+++ 0x04-more_functions_nested_loops/0-main.c
@@ -11,8 +11,8 @@ int main(void)
     char c;
 
     c = 'A';
-    printf("%c: %d\n", c, 0_isupper(c));
+    printf("%c: %d\n", c, _isupper(c));
     c = 'a';
-    printf("%c: %d\n", c, 0_isupper(c));
+    printf("%c: %d\n", c, _isupper(c));
     return (0);
 }
