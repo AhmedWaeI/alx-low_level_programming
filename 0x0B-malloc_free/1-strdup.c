7a108f38f9ec5fe369146ec090c5afe1067e1fdd
xSIayer <ahmedwael130@gmail.com>
2023-04-11 16:52:55 -0700

    asd

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -18,7 +18,7 @@ char *_strdup(char *str)
 		return (NULL);
 	
 	for(i = 0; str[i] != '\0'; i++);
-	p = malloc(sizeof(char) * i);
+	p = malloc(sizeof(char) * (i + 1));
 	for (x = 0; x < i; x++)
 	{
 		p[x] = str[x];
