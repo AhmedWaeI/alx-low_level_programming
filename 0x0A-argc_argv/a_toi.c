35aeaf6f5be68bb7a6c5a2bc90f2fadca10ad361
xSIayer <ahmedwael130@gmail.com>
2023-04-10 18:33:08 -0700

    asd

--- 0x0A-argc_argv/a_toi.c
+++ 0x0A-argc_argv/a_toi.c
@@ -45,30 +45,3 @@ int _atoi(char *s)
 
 	return (n);
 }
-/**
- * main - print the multiply of arguments passed to the program
- * @argc: number of arguments
- * @argv: array of arguments
- *
- * Return: Always 0 (Success)
- */
-int main(int argc, char *argv[])
-{
-	int i;
-	int p;
-
-	p=1;
-	
-	if (argc <= 1)
-	{
-		printf("Error\n");
-		return (1);
-	}
-
-	for (i = 0; i < argc; i++)
-	{
-		p = p * _atoi(argv[i]);
-	}
-	printf("%d\n", p);
-	return (0);
-}	
