b2ef2fab20a17290c75fa2c81ce8fa624255586b
xSIayer <ahmedwael130@gmail.com>
2023-04-11 16:51:17 -0700

    asd

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+
+/**
+ * *_strdup - copies the string given as parameter
+ * @str: string to duplicate
+ *
+ * Return: pointer to the copied string (Success), NULL (Error)
+ */
+char *_strdup(char *str)
+{
+	char *p;
+
+	int i;
+
+	int x;
+
+	if (str == NULL)
+		return (NULL);
+	
+	for(i = 0; str[i] != '\0'; i++);
+	p = malloc(sizeof(char) * i);
+	for (x = 0; x < i; x++)
+	{
+		p[x] = str[x];
+	}
+	p[i] = '\0';
+	return [p];
+}
