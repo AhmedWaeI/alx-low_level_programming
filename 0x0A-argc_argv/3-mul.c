b908f4c693acabd454cb60fc5678ebc4c5d81e70
xSIayer <ahmedwael130@gmail.com>
2023-04-10 18:27:35 -0700

    asd

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - print the multiply of arguments passed to the program
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+	int p;
+
+	p=1;
+	
+	if (argc <= 1)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	for (i = 0; i < argc; i++)
+	{
+		p = p * atoi(argv[i]);
+	}
+	printf("%d\n", p);
+	return (0);
+}	
