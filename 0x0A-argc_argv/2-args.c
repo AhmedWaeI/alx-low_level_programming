ffcadf7e6c08fbfce06b52f5a93981030d4339e8
xSIayer <ahmedwael130@gmail.com>
2023-04-10 18:15:56 -0700

    asd

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - print the arguments passed to the program
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+}
