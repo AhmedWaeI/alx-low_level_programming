14062e24772afd738569d72914900248fe4bf3aa
xSIayer <ahmedwael130@gmail.com>
2023-04-13 18:59:12 -0700

    asd

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -20,6 +20,13 @@ char *_memset(char *s, char b, unsigned int n)
 
 	return (s);
 }
+/**
+ * *_calloc - allocates memory for an array
+ * @nmemb: number of elements in the array
+ * @size: size of each element
+ *
+ * Return: pointer to allocated memory
+ */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	char *ptr;
