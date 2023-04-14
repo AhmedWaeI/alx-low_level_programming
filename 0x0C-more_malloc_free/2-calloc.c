dbfdb482021e6ee96aa7c87eb168b39db3409c5a
xSIayer <ahmedwael130@gmail.com>
2023-04-13 18:57:17 -0700

    asd

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -35,10 +35,6 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 
 	if (ptr == NULL)
 		return (NULL);
-	for (i = 0; i < nmemb; i++)
-	{
-		ptr[i] = 0;
-	}
-	
+	*_memset(ptr,0,n * nmemb);
 	return (ptr);
 }
