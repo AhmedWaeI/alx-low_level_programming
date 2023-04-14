92d2e3f6829800fec56a93d5d08077296eb83eed
xSIayer <ahmedwael130@gmail.com>
2023-04-13 18:55:38 -0700

    asd

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -9,6 +9,17 @@
  *
  * Return: pointer to the memory area s
  */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
+
+	return (s);
+}
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	char *ptr;
