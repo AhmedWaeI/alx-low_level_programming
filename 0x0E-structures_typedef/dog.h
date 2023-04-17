5f7a1b4bf12535d590dc3eb396da4b5fbbe2c3fc
xSIayer <ahmedwael130@gmail.com>
2023-04-17 13:59:46 -0700

    asd

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * free_dog - frees memory allocated for a struct dog
+ * @d: struct dog to free
+ */
+struct dog
+{
+	char* name;
+
+	float age;
+
+	char* owner;
+
+}
