0a018d9afcd6ba2db018ec535abe75d5438dd9ce
xSIayer <ahmedwael130@gmail.com>
2023-04-19 16:35:26 -0700

    asd

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints its own opcodes
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{ return 0;
+}
