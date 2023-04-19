1dab3b5e1ae1a8cb9ec074e6c2b263da417086e2
xSIayer <ahmedwael130@gmail.com>
2023-04-19 13:53:48 -0700

    asd

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,4 +1,3 @@
-#include "function_pointers.h"
 /**
  * print_name - prints a name
  * @name: name to print
@@ -6,6 +5,6 @@
  */
 void print_name(char* name, void (*f)(char*))
 {
-	if (name || f)
+	if (name && f)
 		f(name);
 }
