dedca7729ea9e42f44fe45bc6ae57b9c45ab6dc7
xSIayer <ahmedwael130@gmail.com>
2023-04-20 14:21:49 -0700

    asd

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -19,7 +19,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 
 		if (x == NULL)
 			x = "(nil)";
-		printf("%s", va_arg(args, char *));
+		printf("%s", x);
 		if (separator && (i < (n - 1)))
 			printf("%s", separator);
 	}
