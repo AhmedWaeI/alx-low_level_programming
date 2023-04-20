be01067113758f1ce2de86b0cf1ce0df72174565
xSIayer <ahmedwael130@gmail.com>
2023-04-20 13:54:48 -0700

    asd

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -11,6 +11,10 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_list args;
 
 	va_start(args, n);
+
+	if (!n)
+		return (0);
+
 	for (i = 0; i < n; i++)
 	{
 		printf("%d", va_arg(args, int));
