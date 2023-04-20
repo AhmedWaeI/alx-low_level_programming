d1a42525714d2df936604427945bde69a6691931
xSIayer <ahmedwael130@gmail.com>
2023-04-20 15:07:41 -0700

    asd

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -31,7 +31,7 @@ void print_all(const char * const format, ...)
 				case 's':
 					s = va_arg(args, char *);
 					if (s == NULL)
-						s = "(nil)"
+						s = "(nil)";
 					printf("%s%s", separator, s);
 			}
 			i++;
