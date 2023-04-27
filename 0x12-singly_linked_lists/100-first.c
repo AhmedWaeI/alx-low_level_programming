ffe503e7420f6dba26fee953871383674bc08cab
xSIayer <ahmed_the_best17@yahoo.com>
2023-04-27 12:28:14 -0700

    asd

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,10 @@
+void before(void) __attribute__ ((constructor));
+
+/**
+ * first - prints a sentence before the main
+ * function is executed
+ */
+void before(void)
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n")
+}
