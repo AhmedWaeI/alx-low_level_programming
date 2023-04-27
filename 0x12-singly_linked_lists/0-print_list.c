603cface1bffda9033b204eecf1b5303af036d9f
xSIayer <ahmed_the_best17@yahoo.com>
2023-04-27 11:37:33 -0700

    asd

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -49,33 +49,3 @@ size_t print_list(const list_t* h)
     return (i);
 
 }
-int main(void)
-{
-    list_t* head;
-    list_t* new;
-    list_t hello = { "World", 5, NULL };
-    size_t n;
-
-    head = &hello;
-    new = malloc(sizeof(list_t));
-    if (new == NULL)
-    {
-        printf("Error\n");
-        return (1);
-    }
-    new->str = strdup("Hello");
-    new->len = 5;
-    new->next = head;
-    head = new;
-    n = print_list(head);
-    printf("-> %lu elements\n", n);
-
-    printf("\n");
-    free(new->str);
-    new->str = NULL;
-    n = print_list(head);
-    printf("-> %lu elements\n", n);
-
-    free(new);
-    return (0);
-}
