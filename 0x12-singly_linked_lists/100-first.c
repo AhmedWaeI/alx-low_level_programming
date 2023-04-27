64229fa77f0308843066d675df3794717b525f8f
xSIayer <ahmed_the_best17@yahoo.com>
2023-04-27 12:32:18 -0700

    asd

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -8,5 +8,5 @@ void before(void) __attribute__ ((constructor));
 void before(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
-	printf("I bore my house upon my back!\n"
+	printf("I bore my house upon my back!\n");
 }
