2acde067935d7704cfe4b3c97af92838e025c2ac
xSIayer <ahmed_the_best17@yahoo.com>
2023-04-27 12:12:03 -0700

    asd

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -2,7 +2,23 @@
 #include <string.h>
 #include <stdio.h>
 #include "lists.h"
+/**
+ * strllen - returns the length of a string
+ * @s: string to evaluate
+ *
+ * Return: the length of the string
+ */
+unsigned int strlllen(const char *s)
+{
+	unsigned int i = 0;
 
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+
+	return (i);
+}
 /**
  * add_node - adds a new node at the beginning of a linked list
  * @head: double pointer to the list_t list
@@ -15,12 +31,12 @@ list_t *add_node(list_t **head, const char *str)
 	list_t *new = malloc(sizeof(list_t));
 	if (!head || !new)
 	{
-		return (NULL):
+		return (NULL);
 	}
 	if (str)
 	{
 		new->str = strdup(str);
-		new->len = _strlen(str);
+		new->len = strlllen(str);
 	}
 	new->next = *head;
 	*head = new;
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -25,5 +25,6 @@ list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
 unsigned long int strllen(const char *s);
+unsigned int strlllen(const char *s);
 
 #endif
