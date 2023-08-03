70304a8e89c898268901e6b39f9fd7aeb9bb8882
AhmedWaeI <ahmed_the_best17@yahoo.com>
2023-08-03 13:55:02 -0700

    asd

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -8,13 +8,11 @@
  */
 int clear_bit(unsigned long int *n, unsigned int index)
 {
-	int set_bit(unsigned long int *n, unsigned int index)
-{
 	if (index > 64)
 	{
 		return (-1);
 	}
-	*n = *n ^ (1 << index);
+	*n = *n & ~(1 << index);
 
 	return (1);
 }
