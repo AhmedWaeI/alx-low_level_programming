859eb0c8dd8dd1a333367e4237038f69f66d2881
AhmedWaeI <ahmed_the_best17@yahoo.com>
2023-08-03 12:38:44 -0700

    asd

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -12,22 +12,23 @@
 
 unsigned int binary_to_uint(const char *b)
 {
-    unsigned int result = 0;
-    if (b == NULL)
-    {
-        return (0);
-    }
-    while (*b != '\0')
-    {
-        if (*b != '0' && *b != '1')
-        {
-            return (0);
-        }
-        else
-        {
-            result = (result << 1) + (*b - '0');
-            b++;
-        }
-    }
-    return (result);
+	unsigned int result = 0;
+
+	if (b == NULL)
+	{
+		return (0);
+	}
+	while (*b != '\0')
+	{
+		if (*b != '0' && *b != '1')
+		{
+			return (0);
+		}
+		else
+		{
+			result = (result << 1) + (*b - '0');
+			b++;
+		}
+	}
+	return (result);
 }
